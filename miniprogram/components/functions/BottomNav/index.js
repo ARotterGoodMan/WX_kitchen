238d15c6c273b2fb230fb0421e9541e1bcfb005c
ARotterGoodMan <Shao264419>
2021-11-20 22:43:17 +0800

    切换效果添加

--- miniprogram/components/functions/BottomNav/index.js
+++ miniprogram/components/functions/BottomNav/index.js
@@ -2,7 +2,9 @@
 Component({
     //组件的属性列表
 
-    properties: {},
+    properties: {
+
+    },
 
     //组件的初始数据
 
@@ -12,11 +14,9 @@ Component({
 
     methods: {
         tapTo(e) {
-            console.log(e);
             this.triggerEvent("tapTo", {
                 num: e.currentTarget.id,
             })
-
             return ""
         }
     },
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,36 +5,12 @@ import {envList} from '../../envList.js';
 Page({
     data: {
         envList,
-        search_value: "",
-        search_placeholder: "搜索",
-        scroll_list: 2,
+        tapNum: '1',
     },
-    getSearchValue(e) {
-        console.log(e)
-        if (e.detail.value !== "") {
-            this.setData({
-                search_value: e.detail.value
-            })
-        } else {
-            this.setData({
-                search_placeholder: "搜索"
-            })
-        }
-        return ""
-    },
-    searchClear() {
+    tapTo(e) {
         this.setData({
-            search_placeholder: ""
+            tapNum: e.detail.num,
         })
-        return ""
-    },
-    search() {
-        console.log(this.data.search_value)
-        this.setData({
-            search_value: "",
-            search_placeholder: "搜索"
-        })
-    },
-
+    }
 
 });
