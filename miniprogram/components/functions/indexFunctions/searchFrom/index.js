7a5cd0692d21be639e08ed599f5a0a56dc9f6c8e
ARotterGoodMan <Shao264419>
2021-11-21 17:27:26 +0800

    主页修改

---
+++ miniprogram/components/functions/indexFunctions/searchFrom/index.js
@@ -0,0 +1,44 @@
+// components/functions/searchFrom/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {},
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    getSearchValue(e) {
+      console.log(e)
+      if (e.detail.value !== "") {
+        this.setData({
+          search_value: e.detail.value
+        })
+      } else {
+        this.setData({
+          search_placeholder: "搜索"
+        })
+      }
+      return ""
+    },
+    searchClear() {
+      this.setData({
+        search_placeholder: ""
+      })
+      return ""
+    },
+    search() {
+      console.log(this.data.search_value)
+      this.setData({
+        search_value: "",
+        search_placeholder: "搜索"
+      })
+    },
+  }
+})
